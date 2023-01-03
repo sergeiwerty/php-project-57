2cb722ced004fb1c95aac7867ca802b529f8b1aa
sergeiwerty <dshl.srp@gmail.com>
2023-01-03 14:58:40 +0200

    update db:seed config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        TaskStatus::factory()
+            ->count(4)
+            ->state(new Sequence(
+                ['name' => 'новый'],
+                ['name' => 'в работе'],
+                ['name' => 'на тестировании'],
+                ['name' => 'завершён'],
+            ))
+            ->create();
     }
 }
