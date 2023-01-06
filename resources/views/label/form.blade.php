fb112cfecd7db570237cd2d36afdc1eafeb2486b
sergeiwerty <dshl.srp@gmail.com>
2023-01-06 13:36:45 +0200

    fix label updating functionality

--- resources/views/label/form.blade.php
+++ resources/views/label/form.blade.php
@@ -1,20 +1,18 @@
+@include('flash::message')
 <div>
     {{ Form::label('name', __('label.Label name')) }}
 </div>
 <div class="mt-5">
     {{ Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3']) }}
 </div>
+@foreach($errors->get('name') as $message)
+    <div class="text-rose-600">
+        {{ $message }}
+    </div>
+@endforeach
 <div class="mt-2">
     {{ Form::label('description', __('label.Description')) }}
 </div>
 <div >
     {{ Form::textarea('description', null, ['class' => 'rounded border-gray-300 w-1/3 h-32']) }}
 </div>
-@include('flash::message')
-@if($errors->any())
-    <div class="text-rose-600">
-        @foreach($errors->all() as $error)
-            {{ $error }}
-        @endforeach
-    </div>
-@endif
--- storage/framework/views/8cb227a6e1a26462fc38077ad4de8b9b0add3ca7.php
+++ storage/framework/views/8cb227a6e1a26462fc38077ad4de8b9b0add3ca7.php
@@ -1,3 +1,4 @@
+<?php echo $__env->make('flash::message', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 <div>
     <?php echo e(Form::label('name', __('label.Label name'))); ?>
 
@@ -6,6 +7,12 @@
     <?php echo e(Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3'])); ?>
 
 </div>
+<?php $__currentLoopData = $errors->get('name'); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $message): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <div class="text-rose-600">
+        <?php echo e($message); ?>
+
+    </div>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 <div class="mt-2">
     <?php echo e(Form::label('description', __('label.Description'))); ?>
 
@@ -14,12 +21,4 @@
     <?php echo e(Form::textarea('description', null, ['class' => 'rounded border-gray-300 w-1/3 h-32'])); ?>
 
 </div>
-<?php echo $__env->make('flash::message', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php if($errors->any()): ?>
-    <div class="text-rose-600">
-        <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-            <?php echo e($error); ?>
-
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-    </div>
-<?php endif; ?><?php /**PATH /var/www/html/resources/views/label/form.blade.php ENDPATH**/ ?>
+<?php /**PATH /var/www/html/resources/views/label/form.blade.php ENDPATH**/ ?>
